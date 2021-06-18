2a1dc21e1fcce28ea68bdc2b380cba818c0e3517
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-19 01:03:24 +0300

    Create: interface AbstractRepository.java

---
+++ src/main/java/api/repositories/AbstractRepository.java
@@ -0,0 +1,16 @@
+package api.repositories;
+
+import exeptions.NoRecordException;
+import model.BaseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AbstractRepository <E extends BaseEntity> {
+     Long create(E entity);
+     Optional<E> get(Long id);
+     List<E> getAll();
+     void update(E entity);
+     void delete(Long id) throws NoRecordException;
+
+}
