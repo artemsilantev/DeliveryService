e06814d1e50c515f51eaff48d38c5d6f0dde8511
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-19 01:02:51 +0300

    Create: AbstractDataStorageImpl.java

---
+++ src/main/java/storages/AbstractDataStorageImpl.java
@@ -0,0 +1,30 @@
+package storages;
+
+import api.data.AbstractDataStorage;
+import model.BaseEntity;
+
+import java.util.List;
+
+public abstract class AbstractDataStorageImpl<E extends BaseEntity> implements AbstractDataStorage<E> {
+
+    private Long entityIdSequence;
+
+    private List<E> entities;
+
+    @Override
+    public List<E> getEntities() {
+        return entities;
+    }
+
+    @Override
+    public void setEntities(List<E> entities) {
+       this.entities = entities;
+    }
+
+    @Override
+    public E create(E entity) {
+        entity.setId(entityIdSequence++);
+        entities.add(entity);
+        return entity;
+    }
+}
