ecdef0ce05f5624999654e88ea2dd3b78f62ee6d
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-22 21:41:03 +0300

    Update: create() return value type

--- src/main/java/api/repositories/AbstractRepository.java
+++ src/main/java/api/repositories/AbstractRepository.java
@@ -2,15 +2,13 @@ package api.repositories;
 
 import exeptions.NoRecordException;
 import model.BaseEntity;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface AbstractRepository <E extends BaseEntity> {
-     Long create(E entity);
+     E create(E entity);
      Optional<E> get(Long id);
      List<E> getAll();
      void update(E entity);
      void delete(Long id) throws NoRecordException;
-
 }
