51ecf6eb004d0670b01c59be001c3b24e228a501
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-22 21:51:47 +0300

    Add: method to sort

--- src/main/java/api/services/ShopItemService.java
+++ src/main/java/api/services/ShopItemService.java
@@ -1,6 +1,13 @@
 package api.services;
 
+import model.Product;
+import model.Shop;
 import model.ShopItem;
+import java.io.IOException;
+import java.util.List;
 
 public interface ShopItemService extends AbstractService<ShopItem> {
+    void saveData() throws IOException;
+    List<ShopItem> getByShop(Shop shop);
+    List<ShopItem> getByProduct(Product product);
 }
