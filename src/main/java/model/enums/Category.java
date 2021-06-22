3dd89a4a967b4db787245fe6ded52d1755e50157
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-22 21:45:03 +0300

    Add: categories

--- src/main/java/model/enums/Category.java
+++ src/main/java/model/enums/Category.java
@@ -1,4 +1,23 @@
 package model.enums;
 
+import lombok.Getter;
+
 public enum Category {
+    ELECTRONICS("electronic devices"),
+    COMPUTERS_AND_NETWORKS("computer and network devices"),
+    APPLIANCES("household appliances"),
+    CONSTRUCTION_AND_REPAIR("construction and repair"),
+    HOUSE_AND_GARDEN("House and garden"),
+    AUTO_AND_MOTO("auto and moto"),
+    BEAUTY_AND_SPORTS("beauty and sports"),
+    CHILDREN_AND_MOTHERS("for children and mothers"),
+    WORK_AND_OFFICE("work and office"),
+    MEAL("meal");
+
+    @Getter
+    private final String info;
+
+    Category(String info){
+        this.info = info;
+    }
 }
