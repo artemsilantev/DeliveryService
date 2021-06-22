99be1a17e307b0a68129055118f26d6f49902734
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-22 21:51:18 +0300

    Add: constructor

--- src/main/java/exeptions/NoRecordException.java
+++ src/main/java/exeptions/NoRecordException.java
@@ -1,11 +1,16 @@
 package exeptions;
 
 public class NoRecordException extends Exception {
-    private String entityType;
-    private Long id;
+    private final String entityType;
+    private final Long id;
+
+    public NoRecordException(String entityType, Long id) {
+        this.entityType = entityType;
+        this.id = id;
+    }
 
     @Override
     public String getMessage() {
-        return String.format("%s was not fount by this id (%d)",entityType,id);
+        return String.format("%s was not found by this id (%d)",entityType,id);
     }
 }
