aa8a38f4b927df768a1ed62ac481a66cae2653a0
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-22 21:38:34 +0300

    Add: setSequenceId

--- src/main/java/api/data/AbstractDataStorage.java
+++ src/main/java/api/data/AbstractDataStorage.java
@@ -3,8 +3,13 @@ package api.data;
 import java.util.List;
 import model.BaseEntity;
 
-public interface AbstractDataStorage <E extends BaseEntity> {
+public interface AbstractDataStorage<E extends BaseEntity> {
+
+    void setSequenceId(Long id);
+
     List<E> getEntities();
-    void setEntities(List <E> entities);
+
+    void setEntities(List<E> entities);
+
     E create(E entity);
 }
