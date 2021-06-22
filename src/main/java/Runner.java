0a5ca6bb043c0e30f974c35c271dff8146d79484
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-22 21:48:59 +0300

    Add: method to run program

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,11 @@
+import intitalizers.MainInitializer;
+import menu.client.MenuClient;
+
 public class Runner {
 
     public static void main(String[] args){
 
+        MainInitializer.getInstance().startInitializer();
+        MenuClient.getInstance().run();
     }
 }
