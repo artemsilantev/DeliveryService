027343fe6fbbe1e552e03e7829f6230a89855549
artemsilantev <artem.silantev.2001@gmail.com>
2021-06-22 21:45:39 +0300

    Add: add constructors and override ToString()

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,10 +1,14 @@
 package model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer extends BaseEntity {
    private String firstName;
    private String secondName;
@@ -13,5 +17,24 @@ public class Customer extends BaseEntity {
    private String address;
 
 
+   public Customer(Customer customer){
+      this.setId(customer.getId());
+      this.firstName = customer.firstName;
+      this.secondName = customer.secondName;
+      this.number = customer.number;
+      this.email = customer.email;
+      this.address = customer.address;
+   }
 
+
+
+   @Override
+   public String toString() {
+      return  "Customer"+ '\n'+
+              "firstName: " + firstName + '\n' +
+              "secondName: " + secondName + '\n' +
+              "number: " + number + '\n' +
+              "email: " + email + '\n' +
+              "address: " + address+'\n';
+   }
 }
